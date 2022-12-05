77d671279e16f6f0dcb878237604f74e6d955df7
Sahira-m <sahikmh@gmail.com>
2022-12-05 15:20:17 +0100

    doing

--- src/components/product/ProductItem.js
+++ src/components/product/ProductItem.js
@@ -1,5 +1,18 @@
 import React from "react";
+import "./ProductItem.css";
+export default function ProductItem({props})
+ {
+  
+  
+    return (
+      <div className="main">
+      <div className='productItem' key={props.id}>
+        <p>Name: {props.title}</p>
+        <img src={props.image} alt={props.name} />
+        <p>Price: {props.price}</p>
+        <button className='addToCartBtn'>ADD TO CART</button>
+      </div>
+      </div>
+    )
+  }
 
-export default function ProductItem() {
-  return <div>ProductItem</div>;
-}
--- src/components/product/ProductList.js
+++ src/components/product/ProductList.js
@@ -1,5 +1,44 @@
-import React from "react";
 
-export default function ProductList() {
-  return <div>ProductList</div>;
+import React, {useState, useEffect} from "react";
+import ProductItem from "./ProductItem";
+import "./ProductList.css";
+
+const url = "https://fakestoreapi.com/products";
+export default function ProductList()
+{
+  const [items, setItems] = useState([]);
+
+  function fetchItems() {
+    fetch(url).then((response) => response.json())
+    .then((items) => setItems(items))
+    .catch((error) => console.error(error))
+  }
+  
+  useEffect(() => {
+    fetchItems();
+  }, []);
+  return(
+  <div className="product">
+    {
+    items.slice(0,12).map((item)=> {
+      return<ProductItem props={item} key={item.id}/>
+    }
+    )
+    
+  }
+  </div>
+  );
+
+//    return <div>ProductList
+//     <div className="productList">
+//       {items.slice(0,9).map((item) =>{
+//   return <div key={item.id}> Name: {item.title} Price:{item.price}<img src={item.image} alt="images"></img>
+//   <button className='addToCartBtn' >ADD TO CART</button> </div>;
+// })}
+
+
+
+//     </div>
+//    </div>;
+ 
 }
